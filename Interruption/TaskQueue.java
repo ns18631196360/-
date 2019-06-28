f4d6b9d5ae3e7bbaee7c5ad52729e64176a90e8d
YNS <40994452+ns18631196360@users.noreply.github.com>
2019-06-28 14:58:25 +0800

    Update TaskQueue.java

--- Interruption/TaskQueue.java
+++ Interruption/TaskQueue.java
@@ -4,8 +4,6 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
- * @Author: 无双老师【云析学院】
- * @Date: 2019-05-18
  * @Description: 自定义一个任务队列，封装了LinkedBlockingQueue
  */
 public class TaskQueue<T> {
